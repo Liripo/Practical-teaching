53fc345e10d65660ba95387435dc3da045aa3f18
Liripo <liripo@qq.com>
2020-07-02 23:03:43 +0800

    second

--- R/global.R
+++ R/global.R
@@ -3,6 +3,17 @@ library(shiny)
 library(shinydashboardPlus)
 library(shinydashboard)
 library(shinyWidgets)
-covid_data <- nCov2019::get_nCov2019()
+library(echarts4r)
+library(echarts4r.maps)
+covid_data <- nCov2019::get_nCov2019(lang = "en")
 chinese <- covid_data[]
 global <- covid_data$global
+global_map <- function(data){
+  cns <- countrycode::codelist$country.name.en
+  cns <- data.frame(countrys = cns)
+  cns <- cns %>% left_join(data %>% select(1:2),by = c("countrys" = "name"))
+  cns %>% 
+    e_charts(countrys) %>% 
+    e_map(confirm) %>% 
+    e_visual_map(confirm)
+}
--- app.R
+++ app.R
@@ -1,41 +1,97 @@
-source("R/global.R")
-
-library(shiny)
-ui <- dashboardPagePlus(skin = "red",enable_preloader = T,
+#-------------
+#ui
+#-------------
+ui <- dashboardPagePlus(skin = "yellow-light",
+  enable_preloader = T,#加载
+  loading_duration = 1,
   header = dashboardHeaderPlus(
+    #添加标题隐藏后显示图片
     title = tagList(tags$span("Practical-teaching",class = "logo-lg"),
-                              tags$img(src = "ShinyDashboardPlus_FINAL.svg")),
-    enable_rightsidebar = TRUE,
-    rightSidebarIcon = "gears"
+      tags$img(src = "ShinyDashboardPlus_FINAL.svg"))
   ),
+  #---------
   sidebar = dashboardSidebar(
     sidebarMenu(
-      menuItem("search",tabName = "search",icon = icon("search"))
+      menuItem("Search",tabName = "search",icon = icon("search")),
+      menuItem("File",tabName = "file",icon = icon("file"))
     )
   ),
+  #-------------
   body = dashboardBody(
     tabItems(
       tabItem(
         "search",
-        shinyWidgets::searchInput(inputId = "search",btnSearch = icon("search"),
+        fluidRow(
+          shinyWidgets::searchInput(inputId = "search",btnSearch = icon("search"),
                                   width = "30%"),
-        br(),
-        box(width = 12,
-        DT::DTOutput("search_out")
+        ),
+        fluidRow(
+          DT::DTOutput("search_out"),
+        ),
+        fluidRow(
+          echarts4rOutput("global_map")
+        )
+      ),
+      tabItem(
+        "file",
+        fluidRow(
+          fileInput("file_input",label = "File input")
+        ),
+        fluidRow(
+          boxPlus(
+            title = "file input and down",
+            closable = TRUE, 
+            enable_label = TRUE,
+            label_text = 1,
+            label_status = "danger",
+            status = "warning",
+            solidHeader = FALSE, 
+            collapsible = TRUE,
+            tableOutput("file_out"),
+            downloadBttn("down_out")
+          )
         )
       )
     )
   ),
-  rightsidebar = rightSidebar()
 )
 #------------------------------
+#server
+#------------------------------
 server <- function(input, output, session) {
-  data <- reactive({
-    global %>% filter(name == input$search)
-  })
+  #查看searchInput源代码发现其id是加上_search
+  #---------
+  #表达式reactive
+  #---------
+  observeEvent(input$search_search,{
+    data <- reactive({
+      global %>% 
+       filter(name == input$search)
+    })
+  #----------------
+  #output
+  #---------------
   output$search_out <- DT::renderDataTable({
-    DT::datatable(global,style = "bootstrap4")
+      DT::datatable(global,style = "bootstrap4")
+    })
   })
+  output$global_map <- renderEcharts4r({
+    global_map(data = global)
+  })
+  output$file_out <- renderTable({
+    req(input$file_input)
+    cat(str(input$file_input),"\n")
+    data.table::fread(input$file_input$datapath)
+  })
+  output$down_out <- downloadHandler(
+    filename = function() {
+      paste('data-', Sys.Date(), '.csv', sep='')
+    },
+    content = function(con) {
+      write.csv(mtcars, con)
+    }
+  )
 }
+
 #------------------------------
 shinyApp(ui, server)
